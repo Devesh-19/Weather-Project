5879f09ad4797481e4f903b9ad1e0c8b2b23ad06
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-26 02:10:33 +0530

    Logging weather data for a particular location

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
 app.get ("/", (req, res) =>
 {
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Lucknow&units=metric&APPID=6f5f78be2f380779e52bb13e77db7fe6"
+    https.get(url, response =>
+    {
+        response.on('data', (d) => 
+        {
+            const data = JSON.parse(d);
+            console.log(data);
+        });
+      
+    })
+
     res.send("Hello to this server");
 })
 
