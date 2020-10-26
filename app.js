c9a78c6f4b703d710591ade12aea26e37bf30026
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-26 15:32:17 +0530

    Started taking user input and displaying respective weather data

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+const { Transform } = require("stream");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get ("/", (req, res) =>
 {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Lucknow&units=metric&APPID=6f5f78be2f380779e52bb13e77db7fe6"
-    https.get(url, response =>
-    {
-        response.on('data', (d) => 
-        {
-            const weatherData = JSON.parse(d);
-            const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            res.write(`<h1>The temperature in Lucknow is ${temp} degrees Celsius</h1>`);
-            res.write(`<h3>The weather is ${weatherDescription}</h3>`);
-            
-            res.send();
-        });
-      
-    })
+    res.sendFile(`${__dirname}/index.html`);
+});
 
-})
+app.post("/", (req, res) =>
+{
+        const query = req.body.cityName;
+        const apiKey = "6f5f78be2f380779e52bb13e77db7fe6"
+        const unit = "metric";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit}&APPID=${apiKey}`;
+        https.get(url, response =>
+        {
+            response.on('data', (d) => 
+            {
+                const weatherData = JSON.parse(d);
+                const temp = weatherData.main.temp;
+                const weatherDescription = weatherData.weather[0].description;            
+                const icon = weatherData.weather[0].icon;
+                const imgUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+                res.write(`<h1>The temperature in ${query} is ${temp} degrees Celsius</h1>`);
+                res.write(`<h3>The weather is ${weatherDescription}</h3>`);
+                res.write(`<img src = ${imgUrl}>`);
+                
+                res.send();
+            });            
+        });    
+});
+
 
 
 app.listen(6900, () =>
 {
     console.log("Server is running at port 6900");
-})
+});
