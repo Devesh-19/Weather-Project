60fda8c6514dcf4255a667fda314c40ee9e1a70e
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-27 00:16:49 +0530

    Made the API Key an env variable

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,7 +14,7 @@ app.get ("/", (req, res) =>
 app.post("/", (req, res) =>
 {
         const query = req.body.cityName;
-        const apiKey = "6f5f78be2f380779e52bb13e77db7fe6"       //Fake API key. Generate and use an API key here
+        const apiKey = process.env.API_KEY;
         const unit = "metric";
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit}&APPID=${apiKey}`;
         https.get(url, response =>
